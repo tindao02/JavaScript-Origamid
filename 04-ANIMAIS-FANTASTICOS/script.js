64615325e9e56c4d40a4762415da02bf56a92892
Tindao02 <timdao02@hotmail.com>
2023-08-14 07:27:04 -0300

    eventos

--- 04-ANIMAIS-FANTASTICOS/script.js
+++ 04-ANIMAIS-FANTASTICOS/script.js
@@ -1,51 +1,24 @@
-const section = document.querySelector('.animais');
+const img = document.querySelector('img');
 
-console.log(section.clientHeight);
-console.log(section.offsetHeight);
-console.log(section.scrollHeight);
-
-// tamanho da janela
-console.log('width da janela: ',window.innerWidth);
-console.log('Height da janela: ', window.innerHeight);
-
-console.log('distância total do scroll vertical: ', window.pageYOffset);
-console.log('distância total do scroll vertical: ', window.scrollY);
-
-console.log('distância total do scroll horizontal: ', window.pageXOffset);
-console.log('distância total do scroll horizontal: ', window.scrollX);
-
-// Verifique a distância da primeira imagem
-// em relação ao topo da página
-const img = document.querySelector('img')
-console.log(img.offsetTop);
-
-// Retorne a soma da largura de todas as imagens
-const somaImagens = function() {
-  const imagens = document.querySelectorAll('img');
-  let soma = 0;
-  imagens.forEach(imagem => soma += imagem.offsetWidth);
-  console.log(soma);
+// img.addEventListener('click', () => console.log('clicou'));
+const callback = function(event) {
+  console.log('clicou', event);
 }
+// img.addEventListener('click', callback);
 
-window.onload = function() {
-  somaImagens();
+
+const animaisLista = document.querySelector('.animais-lista');
+const callbackLista = function(event) {
+  console.log(event.currentTarget);
+  console.log(event.target);
+  console.log(event.type);
 }
+// animaisLista.addEventListener('click', callbackLista);
 
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
-// com o dedo. (48px/48px de acordo com o google)
-const links = document.querySelectorAll('a');
 
-links.forEach(link => {
-  if(link.offsetWidth >= 48 && link.offsetHeight >= 48) console.log(link, 'Possui boa acessibilidade');
-  else console.log(link, 'Não possui boa acessibilidade', link.offsetWidth, link.offsetHeight);
-});
-
-// Se o browser for menor que 720px,
-// adicione a classe menu-mobile ao menu
-const browserSmall = window.matchMedia('(max-width: 720px)').matches;
-if(browserSmall) {
-  const menu = document.querySelector('.menu');
-  menu.classList.add('menu-mobile');
+const linkExterno = document.querySelector('a[href^="http"');
+const handleLinkExterno = function(event) {
+  event.preventDefault();
+ console.log('clicou');
 }
-console.log(document.querySelector('.menu').classList);
+linkExterno.addEventListener('click', handleLinkExterno);
